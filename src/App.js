88c8336c7baffffe024a931bcd5e919efc29b87c
Megan DeRisi <Megan@Lauras-MacBook-Pro.local>
2024-02-19 16:18:55 -0600

    framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import About from './pages/about/About';
+import Home from './pages/home/Home';
+import NotFound from './pages/notfound/NotFound';
+import Profile from './pages/profile/Profile';
+import Search from './pages/search/Search';
+import Shelves from './pages/shelves/Shelves';
+import SignUp from './pages/signUp/SignUp';
+import SignIn from './pages/signIn/SignIn';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Header />
+      <Home />
+      <About />
+      <Profile />
+      <Search />
+      <Shelves />
+      <SignUp />
+      <SignIn />
+      <NotFound />
+      <Footer />
+    </div>);
 }
 
 export default App;
